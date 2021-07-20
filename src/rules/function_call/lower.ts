b020cba67cba3d030f5329bafed60beef4c42978
joaovitmac <joaovmachado10@gmail.com>
2021-07-19 21:36:45 -0300

    fix lower function

--- src/rules/function_call/lower.ts
+++ src/rules/function_call/lower.ts
@@ -3,5 +3,5 @@ import { MySqlTransformer } from "../..";
 export default function LOWER(node: any, ctx: MySqlTransformer) {
     const argument_node = node.arguments[0];
     
-    return `FLOOR(${ctx.transform(argument_node)})`;
+    return `LOWER(${ctx.transform(argument_node)})`;
 }
